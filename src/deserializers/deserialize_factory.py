06196f655ee4e77080523f4c359f0115285fb9ca
Alyona1619 <alena.g190503@gmail.com>
2024-10-04 14:02:35 +0800

    commented try to fix deserialization

--- src/deserializers/deserialize_factory.py
+++ src/deserializers/deserialize_factory.py
@@ -1,3 +1,22 @@
+# from src.core.abstract_reference import abstract_reference
+# from src.core.validator import argument_exception
+#
+#
+# class DeserializeFactory:
+#     """Фабрика для создания десериализаторов различных моделей"""
+#
+#     @staticmethod
+#     def get_deserializer(data_type):
+#         subclasses = abstract_reference.__subclasses__()
+#         deserializer_map = {
+#             cls.__name__.lower(): cls() for cls in subclasses
+#         }
+#
+#         if data_type in deserializer_map:
+#             return deserializer_map[data_type]
+#         else:
+#             raise argument_exception(f"Неизвестный тип данных для десериализации: {data_type}")
+
 from src.deserializers.deserializers import NomenclatureDeserializer, GroupDeserializer, RangeDeserializer, IngredientDeserializer
 from src.core.validator import argument_exception
 
--- src/deserializers/json_deserializer.py
+++ src/deserializers/json_deserializer.py
@@ -1,7 +1,7 @@
 from src.models.ingredient import ingredient
 
+
 class JsonDeserializer:
-    """Класс для десериализации рецептов из JSON через фабричные методы моделей."""
 
     @staticmethod
     def deserialize_recipe(json_data):
@@ -10,3 +10,18 @@ class JsonDeserializer:
             ingredient_instance = ingredient.from_json(item)
             recipe_list.append(ingredient_instance)
         return recipe_list
+
+
+#from src.deserializers.deserialize_factory import DeserializeFactory
+#
+#
+# class JsonDeserializer:
+#     """Класс для десериализации рецептов из JSON через фабричные методы моделей."""
+#
+#     @staticmethod
+#     def deserialize_recipe(json_data):
+#         recipe_list = []
+#         for item in json_data:
+#             ingredient_instance = DeserializeFactory.get_deserializer('ingredient').deserialize(item)
+#             recipe_list.append(ingredient_instance)
+#         return recipe_list
